60fe8d072a2e3104672a0eb89e7e60358963dc77
Tejas321 <72182805+Tejas321@users.noreply.github.com>
2023-08-20 11:11:02 +0530

    changes

--- P1_ifElse.java
+++ P1_ifElse.java
@@ -6,28 +6,22 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
-
-
+public class P1_ifElse {
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        if(N%2!=0){
+        if (N % 2 != 0) {
             System.out.println("Weird");
+        } else if (N % 2 == 0 && (1 < N && N < 6)) {
+            System.out.println("Not Weird");
+        } else if (N % 2 == 0 && (5 < N && N < 21)) {
+            System.out.println("Weird");
+        } else if (N % 2 == 0 && N > 20) {
+            System.out.println("Not Weird");
         }
-        else if(N%2==0 && (1<N && N<6)){
-            System.out.println("Not Weird");   
-        }
-        else if(N%2==0 && (5<N && N<21)){
-            System.out.println("Weird");   
-        }
-        else if(N%2==0 && N>20){
-            System.out.println("Not Weird");   
-        }
-        
 
         scanner.close();
     }
