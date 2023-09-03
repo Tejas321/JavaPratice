28a66cd1b90eada6bc06c9da8a398f4e9934372e
Tejas321 <72182805+Tejas321@users.noreply.github.com>
2023-09-03 20:51:08 +0530

    Binary Search

---
+++ P20_binarySearch.java
@@ -0,0 +1,26 @@
+public class P20_binarySearch {
+    public static void main(String[] args) {
+        char[] arr = { 'a', 'b', 'c' };
+        char target = 'c';
+        char ch = nextGreatestLetter(arr, target);
+        System.out.println(ch);
+
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+
+            }
+        }
+        return letters[start % letters.length];
+
+    }
+}
