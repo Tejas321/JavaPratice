eddd68cd460b1c49f21433f4e0f27a9da6d6ed0a
Tejas321 <72182805+Tejas321@users.noreply.github.com>
2023-09-04 12:16:23 +0530

    Binary search in infinte array

---
+++ P21_infiniteArrayBS.java
@@ -0,0 +1,38 @@
+public class P21_infiniteArrayBS {
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170 };
+        int target = 10;
+        int ans = findingRange(arr, target);
+        System.out.println(ans);
+    }
+
+    static int findingRange(int[] arr, int target) {
+        // find findingRange
+        int start = 0;
+        int end = 1;
+        while (target > arr[end]) {
+            int temp = end + 1;
+            // end=perious end+sizeofbox*2
+            end = end + 2 * (end - start + 1);
+            start = temp;
+
+        }
+        return binarySearch(arr, start, end, target);
+
+    }
+
+    static int binarySearch(int[] arr, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+}
