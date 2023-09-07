228b89c8244b5f509bc5d3a5679d7407dace8c67
Tejas321 <72182805+Tejas321@users.noreply.github.com>
2023-09-07 09:14:55 +0530

    problem

---
+++ P23_findInMountainArray.java
@@ -0,0 +1,55 @@
+public class P23_findInMountainArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 3, 1 };
+        int target = 3;
+        int ans = peakIndexInMountainArray(arr);
+        int start = 0;
+        int end = arr.length - 1;
+        int firsttry = position(arr, target, start, ans);
+        if (firsttry != -1) {
+            System.out.println(firsttry);
+        } else {
+            int secondttry = position(arr, target, ans, end);
+            System.out.println(secondttry);
+        }
+
+    }
+
+    static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+
+    // at last start and end are pointing to same
+    static int position(int[] arr, int target, int start, int end) {
+        boolean isAsc = arr[start] < arr[end];
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            } else if (isAsc) {
+                if (target > arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
