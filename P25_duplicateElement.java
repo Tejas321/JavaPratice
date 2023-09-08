5b69f485880d48f961cd4b2d8e6bfd9d835cbf48
Tejas321 <72182805+Tejas321@users.noreply.github.com>
2023-09-08 07:15:52 +0530

    duplicate element problem

---
+++ P25_duplicateElement.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class P25_duplicateElement {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return nums[i];
+            }
+
+        }
+        return nums[nums.length - 1];
+    }
+
+}
